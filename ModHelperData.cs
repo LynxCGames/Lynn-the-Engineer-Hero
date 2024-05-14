264b65ea00dcb2b90c3dd192fa05b7abc2e67dd0
Matthew Wojnicki <mwojnicki@comcast.net>
2024-05-14 10:09:01 -0500

    v1.0.0

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,9 +5,10 @@ public static class ModHelperData
     public const string WorksOnVersion = "42.3";
     public const string Version = "1.0.0";
     public const string Name = "Lynn the Hero";
+    public const string Author = "LynxC";
 
     public const string Description = "A mod based on a hero concept by SHIIRAFISH on the btd6 subreddit.";
 
-    public const string RepoOwner = "LyncCGames"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "LynxCGames";
+    public const string RepoName = "Lynn-the-Engineer-Hero";
 }
